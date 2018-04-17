6135d2419414ea79617103d0ecbe18926e3da294
jartigag <jartigag@protonmail.com>
2018-04-17 20:20:59 +0200

    testing script

--- helloinstaworld.py
+++ helloinstaworld.py
@@ -3,3 +3,5 @@ from credentials import insta_username, insta_password
 
 session = InstaPy(username=insta_username, password=insta_password)
 session.login()
+session.follow_user_followers(['spotify'], amount=10, randomize=True, sleep_delay=60) #by default, sleep_delay=600
+#TODO: myPic = lastUploadedPic
